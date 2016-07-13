185eeea362d6efe4da202f6409cad2f9bebaf611
Mark Barnes <mark@sharonandmark.net>
2016-07-13 16:04:31 +0100

    In facebook_instant_articles.php, deleted the 'remove_onpage_hyperlinks' filter, as this is now included in the Facebook Instant Articles plugin 3.0.

--- classes/facebook_instant_articles.php
+++ classes/facebook_instant_articles.php
@@ -16,7 +16,6 @@ class evangelical_magazine_facebook_instant_articles {
     public function __construct() {
         add_filter ('instant_articles_post_types', create_function('', 'return array ("em_article");'));
         add_filter ('instant_articles_authors', array (__CLASS__, 'filter_author'));
-        add_filter ('instant_articles_content', array (__CLASS__, 'remove_onpage_hyperlinks'));
     }
 
     /**
@@ -33,21 +32,5 @@ class evangelical_magazine_facebook_instant_articles {
             $author[0]->display_name = $object->get_author_names();
         }
         return $author;
-    }
-
-    /**
-    * Filters the content for instant articles to remove onpage hyperlinks
-    * 
-    * (.e.g. hyperlinks that begin with a #)
-    * 
-    * @param string $content
-    * @return string
-    */
-    public static function remove_onpage_hyperlinks ($content) {
-        preg_match_all( '!<a[^>]*? href=[\'"]#[^<]+</a>!i', $content, $matches );
-        foreach ( $matches[0] as $link ) {
-                $content = str_replace( $link, strip_tags($link), $content );
-        }
-        return $content;
     }
 }
